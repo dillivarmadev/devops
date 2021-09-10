c5f05ce9f8304f26ae8d96d549d7cfa27b35317e
bhasvara <bhasvara@cisco.com>
2021-09-10 07:49:18 +0530

    update perl program and added windows batch program

---
+++ batch_programs/win_bat_one.bat
@@ -0,0 +1,9 @@
+@echo off 
+Rem Listing all the files in the directory Program files 
+echo "My name is H Dilli Varma Garu"
+@echo on
+echo %PATH%
+@echo off
+echo %TITLE%
+echo
+echo "Done!"
--- perl/perl_program.sh
+++ perl/perl_program.sh
@@ -1,9 +1,17 @@
 
-print "my name is dilli\n";
+print "My name is Dilli, and Iam not an expert DevOPs engineer yet.\n\n";
 print "This is my program to print  1 - 10\n";
 $count = 1;
 while ( $count <= 10 ) {
-	printf "$count\n";
+	printf "$count ";
 	$count = $count + 1;
 }
+printf("\n\n");
+print "This is my program to print  10 - 1\n";
+$down_count = 10;
+while ( $down_count >= 1 ) {
+	printf("$down_count ");
+	$down_count = $down_count - 1;
+}
+printf("\n")
 
